a95a6206a6c2c8e31034df46b2cbe699b1dcb416
Timothy <timothylam96@hotmail.com>
2020-05-14 01:58:21 +0800

    done

--- main.go
+++ main.go
@@ -3,43 +3,60 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"math"
 	"os"
 	"runtime"
 	"strconv"
 	"strings"
-	"sync/atomic"
+	"time"
 )
 
-var chResult = make(chan uint64)
+var chResult = make(chan largeNumber)
 var chInput = make(chan bool)
-var input uint64
-var caches = make([]cache, 1)
+var input largeNumber
+var caches = make(map[string]cache)
 
 type cache struct {
-	totalSum uint64
+	totalSum largeNumber
 }
+
+// Max length of max int
+type largeNumber struct {
+	numbers []byte
+}
+
+var largeNumberOne largeNumber
+var largeNumberZero largeNumber
+
+var start time.Time
 
 // Make a 3 GB 'ballast'
 // var _ = make([]byte, 10<<30)
 
+func init() {
+	largeNumberOne = newLargeNumber("1")
+	largeNumberZero = newLargeNumber("0")
+}
+
 func main() {
-	var answer uint64
+	var answer largeNumber
 	// Start concurrent calculation
-	go backgroundRunner()
+	go backgroundCalcPowerSum()
 
 	// Get user input concurrently
 	go getUserInput()
 
 	// Wait for the result
 	answer = <-chResult
+	elapsed := time.Since(start)
+	fmt.Printf("Time used: %f seconds\n", elapsed.Seconds())
 
 	// Print the result
-	fmt.Printf("Result is: %d\n", answer)
+	// fmt.Printf("Result is: %s\n", answer.String())
+	fmt.Println("Result is:")
+	answer.Print()
 }
 
 func getUserInput() {
-	var userInput int
 	r := bufio.NewReader(os.Stdin)
 
 	// Retrieve input
@@ -50,86 +67,295 @@ func getUserInput() {
 	}
 
 	// Set user input
-	userInput, err = strconv.Atoi(strings.TrimSpace(userInputStr))
-	setUserInput(userInput)
+	setUserInput(newLargeNumber(strings.TrimSpace(userInputStr)))
 }
 
-func setUserInput(userInput int) {
-	if userInput < 0 {
-		panic("Negative numbers are not allowed:" + strconv.Itoa(userInput))
+func setUserInput(userInput largeNumber) {
+	if userInput.IsLessThan(largeNumberZero) {
+		panic("Negative numbers are not allowed:" + userInput.String())
 	}
 
-	if userInput >= math.MaxInt64 {
-		panic("Input number too large.")
-	}
-
-	atomic.StoreUint64(&input, uint64(userInput))
+	// atomic.StoreUint64(&input, uint64(userInput))
+	input = userInput
 
 	// Signal input received and store
 	chInput <- true
+
+	// Start timer after receiving and verifying user input ;D
+	start = time.Now()
 }
 
-func pow(n, p uint64) (a uint64) {
-	if n == 1 || p == 1 {
-		return 1
-	}
-	if n == 0 {
-		return 0
+func pow(n, p largeNumber) (a largeNumber) {
+	if n.Length() == 1 && n.GetAsInt(0) == 1 {
+		return newLargeNumber("1")
 	}
 
-	a = n
-	var i uint64 = 1
+	if p.Length() == 1 && p.GetAsInt(0) == 1 {
+		return newLargeNumber("1")
+	}
 
-	for ; i < p; i++ {
-		a *= n
+	if n.Length() == 1 && n.GetAsInt(0) == 0 {
+		return newLargeNumber("0")
+	}
+
+	a = newLargeNumber(n.String())
+	i := newLargeNumber("1")
+
+	for ; i.IsLessThan(p); i.IncrementByOne() {
+		a.Multiply(n)
 	}
 
 	return
 }
 
-func backgroundRunner() {
-	backgroundCalcPowerSum()
-	calcPowerSum()
-}
-
 func backgroundCalcPowerSum() {
 	// Start of sum is 0
-	var sum uint64 = 0
-	var cur uint64 = 1
+	sum := newLargeNumber("0")
+	cur := newLargeNumber("1")
+	var hasUserInput bool
 
 	for {
-		sum += pow(cur, cur)
+		sum.Add(pow(cur, cur))
 
-		caches = append(caches, cache{
-			totalSum: sum,
-		})
+		if !hasUserInput {
+			caches[cur.String()] = cache{
+				totalSum: newLargeNumber(sum.String()),
+			}
+		}
 
-		cur++
+		cur.IncrementByOne()
 
-		select {
-		case <-chInput:
-			return
-		default:
-			// Make sure the goroutine to get user input is running
-			runtime.Gosched()
+		if hasUserInput {
+			// Check if user solution is already calculated
+			if cur.IsGreaterThan(input) {
+				chResult <- caches[input.String()].totalSum
+				return
+			} else if cur.IsEqual(input) {
+				chResult <- sum
+			}
+		} else {
+			select {
+			case <-chInput:
+				hasUserInput = true
+			default:
+				// Make sure the goroutine to get user input is running
+				runtime.Gosched()
+			}
 		}
 	}
 }
 
-func calcPowerSum() {
-	// Check if user solution is already calculated
-	if len(caches) >= int(input) {
-		chResult <- caches[input].totalSum
+func newLargeNumber(s string) largeNumber {
+	maxLen := len(s)
+	newBytes := make([]byte, maxLen)
+
+	for i, j := 0, maxLen-1; i < maxLen; i++ {
+		newBytes[i] = s[j]
+		j--
+	}
+
+	return largeNumber{numbers: newBytes}
+}
+
+func (n *largeNumber) String() string {
+	var strBuilder strings.Builder
+	for i := n.Length() - 1; i >= 0; i-- {
+		strBuilder.WriteByte(n.numbers[i])
+	}
+
+	return strBuilder.String()
+}
+
+func (n *largeNumber) Append(s string) {
+	maxLen := len(s)
+	newBytes := make([]byte, maxLen)
+
+	for i, j := 0, maxLen-1; i < maxLen; i++ {
+		newBytes[i] = s[j]
+		j--
+	}
+
+	n.numbers = append(n.numbers, newBytes...)
+}
+
+func (n *largeNumber) Replace(s string) {
+	maxLen := len(s)
+	newBytes := make([]byte, maxLen)
+
+	for i, j := 0, maxLen-1; i < maxLen; i++ {
+		newBytes[i] = s[j]
+		j--
+	}
+
+	n.numbers = newBytes
+}
+
+func (n *largeNumber) IsLessThan(c largeNumber) bool {
+	// Return true if n is less than c
+	if n.Length() < c.Length() {
+		return true
+	} else if n.Length() > c.Length() {
+		return false
+	}
+
+	for k := n.Length() - 1; k >= 0; k-- {
+		if n.GetAsInt(k) < c.GetAsInt(k) {
+			return true
+		} else if n.GetAsInt(k) > c.GetAsInt(k) {
+			return false
+		}
+	}
+
+	return false
+}
+
+func (n *largeNumber) IsGreaterThan(c largeNumber) bool {
+	// Return true if n is greater than c
+	if n.IsLessThan(c) || n.IsEqual(c) {
+		return false
+	}
+
+	return true
+}
+
+func (n *largeNumber) IsEqual(c largeNumber) bool {
+	if n.IsLessThan(c) || c.IsLessThan(*n) {
+		return false
+	}
+
+	return true
+}
+
+func (n *largeNumber) Get(i int) byte {
+	return n.numbers[i]
+}
+
+func (n *largeNumber) GetAsInt(i int) int {
+	c, _ := strconv.Atoi(string(n.numbers[i]))
+	return c
+}
+
+func (n *largeNumber) Set(i int, v int) {
+	if v >= 10 {
+		panic("Input more than 10")
+	}
+
+	n.numbers[i] = []byte(strconv.Itoa(v))[0]
+}
+
+func (n *largeNumber) Length() int {
+	return len(n.numbers)
+}
+
+func (n *largeNumber) IncrementByOne() {
+	n.Add(largeNumberOne)
+}
+
+func (n *largeNumber) Add(l largeNumber) {
+	if l.Length() > n.Length() {
+		// Swap if input is bigger than instance
+		temp := l.String()
+		l.Add(*n)
+		n.Replace(l.String())
+		l.Replace(temp)
 		return
 	}
 
-	// Start from last calculated power sum up to user input
-	lastPowerSumNum := len(caches) - 1
-	sum := caches[lastPowerSumNum].totalSum
+	// Add l to n
+	// bring forward holder // 0 to 9
+	bf := 0
+	for k, k1 := 0, 0; k < n.Length(); k++ {
+		cur := n.GetAsInt(k)
+		var curNew int
 
-	for i := uint64(lastPowerSumNum) + 1; i <= input; i++ {
-		sum += pow(i, i)
+		if k1 < l.Length() {
+			curNew = l.GetAsInt(k1)
+		} else {
+			// Add the bring forward if any
+			curNew = bf
+			bf = 0
+
+			if curNew == 0 {
+				break
+			}
+		}
+
+		if cur+curNew+bf >= 10 {
+			newSum := cur + curNew + bf
+			newNum := newSum % 10
+
+			n.Set(k, newNum)
+			bf = newSum / 10
+		} else {
+			n.Set(k, cur+curNew+bf)
+			bf = 0
+		}
+
+		k1++
 	}
 
-	chResult <- sum
+	if bf > 0 {
+		n.Append(strconv.Itoa(bf))
+	}
+}
+
+func (n *largeNumber) Multiply(l largeNumber) {
+	if l.Length() == 0 {
+		panic("No input specified in multiplication")
+	}
+
+	// Multiply l with n
+	// bring forward holder // 0 to 9
+	bf := 0
+	var newMulti []largeNumber
+
+	for k1 := 0; k1 < l.Length(); k1++ {
+		newLN := newLargeNumber("")
+
+		for i := 0; i < k1; i++ {
+			newLN.Append("0")
+		}
+
+		for k := 0; k < n.Length(); k++ {
+			newSum := l.GetAsInt(k1)*n.GetAsInt(k) + bf
+
+			if newSum >= 10 {
+				newNum := newSum % 10
+				newLN.Append(strconv.Itoa(newNum))
+				bf = newSum / 10
+			} else {
+				newLN.Append(strconv.Itoa(newSum))
+				bf = 0
+			}
+		}
+		if bf > 0 {
+			newLN.Append(strconv.Itoa(bf))
+			bf = 0
+		}
+
+		newMulti = append(newMulti, newLN)
+	}
+
+	if len(newMulti) >= 1 {
+		for k := range newMulti[1:] {
+			newMulti[0].Add(newMulti[k+1])
+		}
+
+		n.Replace(newMulti[0].String())
+	}
+}
+
+func (n *largeNumber) Print() {
+	var strBuilder strings.Builder
+	for i := n.Length() - 1; i >= 0; i-- {
+		strBuilder.WriteByte(n.numbers[i])
+		if i%3 == 0 {
+			strBuilder.WriteRune(',')
+		}
+	}
+
+	finalStr := strBuilder.String()
+	finalStr = finalStr[:len(finalStr)-1]
+
+	fmt.Println(finalStr)
 }
--- main_test.go
+++ main_test.go
@@ -4,35 +4,184 @@ import (
 	"testing"
 )
 
-func TestPow2(t *testing.T) {
-	want := 4
-	get := pow(2, 2)
+// func TestPow2(t *testing.T) {
+// 	want := newLargeNumber("4")
+// 	a := newLargeNumber("2")
+// 	get := pow(a, a)
+//
+// 	if want.String() != get.String() {
+// 		t.Errorf("Error in TestPow. Want %s, Get %s", want.String(), get.String())
+// 	}
+// }
+//
+// func TestPow3(t *testing.T) {
+// 	want := newLargeNumber("27")
+// 	a := newLargeNumber("3")
+// 	get := pow(a, a)
+//
+// 	if want.String() != get.String() {
+// 		t.Errorf("Error in TestPow. Want %s, Get %s", want.String(), get.String())
+// 	}
+// }
+//
+// func TestPow5(t *testing.T) {
+// 	want := newLargeNumber("3125")
+// 	a := newLargeNumber("5")
+// 	b := newLargeNumber("5")
+// 	get := pow(b, a)
+//
+// 	if want.String() != get.String() {
+// 		t.Errorf("Error in TestPow. Want %s, Get %s", want.String(), get.String())
+// 	} else {
+// 		t.Log("Get", get.String())
+// 	}
+// }
+//
+// func TestPow12(t *testing.T) {
+// 	want := newLargeNumber("8916100448256")
+// 	a := newLargeNumber("12")
+// 	b := newLargeNumber("12")
+// 	get := pow(b, a)
+//
+// 	if want.String() != get.String() {
+// 		t.Errorf("Error in TestPow. Want %s, Get %s", want.String(), get.String())
+// 	} else {
+// 		t.Log("Get", get.String())
+// 	}
+// }
 
-	if want != get {
-		t.Errorf("Error in TestPow. Want %d, Get %d", want, get)
+func TestPow20(t *testing.T) {
+	want := newLargeNumber("104857600000000000000000000")
+	a := newLargeNumber("20")
+	b := newLargeNumber("20")
+	get := pow(b, a)
+
+	if want.String() != get.String() {
+		t.Errorf("Error in TestPow. Want %s, Get %s", want.String(), get.String())
+	} else {
+		t.Log("Get", get.String())
 	}
 }
 
-func TestPow3(t *testing.T) {
-	want := 27
-	get := pow(3, 3)
-
-	if want != get {
-		t.Errorf("Error in TestPow. Want %d, Get %d", want, get)
-	}
-}
-
-func TestPow5(t *testing.T) {
-	want := 125
-	get := pow(5, 3)
-
-	if want != get {
-		t.Errorf("Error in TestPow. Want %d, Get %d", want, get)
-	}
-}
-
-func BenchmarkPow(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		pow(5, 5)
-	}
-}
+// func BenchmarkPow(b *testing.B) {
+// 	x := newLargeNumber("5")
+// 	for i := 0; i < b.N; i++ {
+// 		pow(x, x)
+// 	}
+// }
+//
+// func TestLargeNumber_Add(t *testing.T) {
+// 	ln := newLargeNumber("100")
+// 	ln1 := newLargeNumber("50")
+// 	ln.Add(ln1)
+//
+// 	lnAns := newLargeNumber("150")
+// 	get := ln.String()
+// 	want := lnAns.String()
+//
+// 	if want != get {
+// 		t.Errorf("Error in TestLargeNumber. Want %s, Get %s", want, get)
+// 	} else {
+// 		t.Log("Passed. Got:", get)
+// 	}
+// }
+//
+// func TestLargeNumber_Add1(t *testing.T) {
+// 	first := "999999999999999999999999999999999999999"
+// 	second := "2"
+// 	ln := newLargeNumber(first)
+// 	ln1 := newLargeNumber(second)
+// 	ln.Add(ln1)
+//
+// 	get := ln.String()
+// 	want := "1000000000000000000000000000000000000001"
+//
+// 	if want != get {
+// 		t.Errorf("Error in TestLargeNumber. Want %s, Get %s", want, get)
+// 	} else {
+// 		t.Logf("Passed. Want %s + %s. Got %s", first, second, get)
+// 	}
+// }
+//
+// func TestLargeNumber_Add2(t *testing.T) {
+// 	first := "3125"
+// 	second := "288"
+// 	ln := newLargeNumber(first)
+// 	ln1 := newLargeNumber(second)
+// 	ln.Add(ln1)
+//
+// 	get := ln.String()
+// 	want := "3413"
+//
+// 	if want != get {
+// 		t.Errorf("Error in TestLargeNumber. Want %s, Get %s", want, get)
+// 	} else {
+// 		t.Logf("Passed. Want %s + %s. Got %s", first, second, get)
+// 	}
+// }
+//
+// func TestLargeNumber_Add3(t *testing.T) {
+// 	first := "8916100448256"
+// 	second := "295716741928"
+//
+// 	ln := newLargeNumber(first)
+// 	ln1 := newLargeNumber(second)
+// 	ln.Add(ln1)
+//
+// 	get := ln.String()
+// 	want := "9211817190184"
+//
+// 	if want != get {
+// 		t.Errorf("Error in TestLargeNumber. Want %s, Get %s", want, get)
+// 	} else {
+// 		t.Logf("Passed. Want %s + %s. Got %s", first, second, get)
+// 	}
+// }
+//
+// func TestLargeNumber_Multiply(t *testing.T) {
+// 	// First number
+// 	ln := newLargeNumber("1000000000000000000000000000000000000000000000000000000000000000000")
+//
+// 	// Second number
+// 	ln1 := newLargeNumber("2")
+//
+// 	// Multiple them
+// 	ln.Multiply(ln1)
+//
+// 	// Wanted answer
+// 	lnAns := newLargeNumber("2000000000000000000000000000000000000000000000000000000000000000000")
+//
+// 	// Set get and want
+// 	get := ln.String()
+// 	want := lnAns.String()
+//
+// 	if want != get {
+// 		t.Errorf("Error in TestLargeNumber multiple. Want %s, Get %s", want, get)
+// 	} else {
+// 		t.Log("Passed. Got:", get)
+// 	}
+// }
+//
+// func TestLargeNumber_Multiply1(t *testing.T) {
+// 	// First number
+// 	ln := newLargeNumber("5159780352")
+//
+// 	// Second number
+// 	ln1 := newLargeNumber("12")
+//
+// 	// Multiple them
+// 	ln.Multiply(ln1)
+//
+// 	// Wanted answer
+// 	lnAns := newLargeNumber("61917364224")
+//
+// 	// Set get and want
+// 	get := ln.String()
+// 	want := lnAns.String()
+//
+// 	if want != get {
+// 		t.Errorf("Error in TestLargeNumber multiple. Want %s, Get %s", want, get)
+// 	} else {
+// 		t.Log("Passed. Got:", get)
+// 	}
+// }
